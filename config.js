19cb29284069b165dc1e117267c5932696c893e8
renderLK <rendermail600@gmail.com>
2025-09-04 14:20:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~RcQGGJyK#TFp13rfQVuNPboN2ruHgsV_ekVdBvCqM3ZrZslxB3TQ'
 };
